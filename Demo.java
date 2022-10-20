ec6f685101ca8368e6baa0329e3fbf140aaddd95
Seuder147 <113107699+Seuder147@users.noreply.github.com>
2022-10-20 10:30:12 +0200

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,9 @@
+public class Demo {
+    public static void main(String[] args) {
+        ShapeMaker shapeMaker = new ShapeMaker();
+
+        shapeMaker.drawSquare();
+        shapeMaker.drawRectangle();
+        shapeMaker.drawCircle();
+    }
+}
